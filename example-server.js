a7cb16a9724832fee04698ccee9113c8b6cba7b0
Shiftking <dylan.t.rush0@gmail.com>
2016-03-18 17:32:11 -0600

    Added http server to transmit source for the client

--- example-server.js
+++ example-server.js
@@ -15,9 +15,9 @@
 
 "use strict"; // http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
 var mysql = require('mysql'),pollingTimer;
-var express = require('express');
-var app = express();
-var path = require("path");
+//var express = require('express');
+var fs = require('fs');
+var url = require('url');
 
 var connection = mysql.createConnection({
 	host:'localhost',
@@ -57,10 +57,34 @@ var WebSocketServer = require('ws').Server;
 var http = require('http');
 var connectionsArray = [];
 var ws_server = http.createServer();
-app.get('/',function(req,res){
-	res.sendFile(path.join(__dirname+'/index.html'));
-});
-app.listen(3000);
+http.createServer(function(request, response) {
+	// Parse the request containing file name
+   var pathname = url.parse(request.url).pathname;
+
+   // Print the name of the file for which request is made.
+   console.log("Request for " + pathname + " received.");
+
+   // Read the requested file content from file system
+   fs.readFile(pathname.substr(1), function (err, data) {
+      if (err) {
+         console.log(err);
+         // HTTP Status: 404 : NOT FOUND
+         // Content Type: text/plain
+         response.writeHead(404, {'Content-Type': 'text/html'});
+      }else{
+         //Page found
+         // HTTP Status: 200 : OK
+         // Content Type: text/plain
+         response.writeHead(200, {'Content-Type': 'text/html'});
+
+         // Write the content of the file to response body
+         response.write(data.toString());
+      }
+      // Send the response body
+      response.end();
+   });
+}).listen(8081);
+
 var wss = new WebSocketServer({server: ws_server, path: '/foo'});
 wss.on('connection', function(ws) {
 		connectionsArray.push(ws);
